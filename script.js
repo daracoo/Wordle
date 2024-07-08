268b0c5cedae0ac4a665d6b97d05de109d8d0503
daracoo <dkuzmanoski21@gmail.com>
2024-07-08 16:07:33 +0200

    Added taking the word from API

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const ANSWER_LENGTH = 5;
 async function init(){
     let currentGuess = '';
     let currentRow = 0;
+
+
+    const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const resObj = await res.json();
+    const word = resObj.word.toUpperCase();
+    setLoading(false);
+
     function addLetter (letter){
         if(currentGuess.length < ANSWER_LENGTH){
             currentGuess += letter;//dodava bukva
@@ -52,6 +59,10 @@ async function init(){
 
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
+}
+
+function setLoading(isLoading){ // da ja nema ikonata otkako ce se vcita stranata
+    loadingDiv.classList.add('hidden', !isLoading);
 }
 
 init();
